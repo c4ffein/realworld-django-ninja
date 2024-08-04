57247fab54929ef5fd48e16f6ab95151c4d67009
c4ffein <c4ffein>
2024-08-05 01:17:38 +0200

    tryfix-ci

--- helpers/exceptions.py
+++ helpers/exceptions.py
@@ -6,7 +6,6 @@ def clean_integrity_error(error):
     """Helper to convert an IntegrityError from psycopg2/sqlite3 to a field name string"""
     try:
         if isinstance(error.__cause__, UniqueViolation):
-            raise Exception("YOU ARE TESTING POSTGRES")
             return error.__cause__.args[0].split(":")[1].split("(")[1].split(")")[0]
         if isinstance(error.__cause__, IntegrityError):
             return error.__cause__.args[0].split(": ")[1].split(".")[1]
